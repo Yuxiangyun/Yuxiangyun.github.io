7c54bd9661b5ef9ed1608b9f6a8c3830d0eba1ee
xiangyun <3207904833@qq.com>
2025-11-15 00:20:49 +0800

    Site updated: 2025-11-15 00:20:49

--- script/post.js
+++ script/post.js
@@ -1,6 +1,7 @@
 (function($){
-  var $header = $('#header');
-  if (window.innerWidth < 480 && !$header.hasClass('slide-hide')) {
-    $header.addClass('slide-hide');
-  }
+  // 移除移动端自动隐藏 header 的逻辑，确保 header 在所有设备上都能显示
+  // var $header = $('#header');
+  // if (window.innerWidth < 480 && !$header.hasClass('slide-hide')) {
+  //   $header.addClass('slide-hide');
+  // }
 })(jQuery);
